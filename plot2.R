119b28259b57bde1fc233874553c7feb2c3ac4b2
jtarrou <melvintheprofessional@gmail.com>
2014-11-08 13:04:54 -0600

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -4,7 +4,7 @@
 # First read the data
 myData <- read.csv("./power_cons/h_P_C.txt", header=TRUE, sep=";", na.strings="?", nrows=2075259, check.names=FALSE, stringsAsFactors=FALSE, comment.char= "", quote='\"')
 
-# let R know what you mean by Dates 
+# use as.Date to set $Date variable 
 myData$Date <- as.Date(myData$Date, format="%d/%m/%Y")
 
 # get the dates required for the project: Feb. 1-2, 2007
@@ -16,7 +16,7 @@ dateTime <- paste(as.Date(data_for_Dates$Date), data_for_Dates$Time)
 # the subset, use POSIXct to make sure objects in data_for_Dates$Datetime are proper class
 data_for_Dates$Datetime <- as.POSIXct(dateTime)
 
-# get line plot
+# get line plot 
 plot(data_for_Dates$Global_active_power ~ data_for_Dates$Datetime, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)") 
 
 # save file as directed 	 
